4470329dd1090435c68eba98582ad5c0355f2b9f
Vitucc <43967523+Vitucc@users.noreply.github.com>
2019-09-14 12:02:03 +0200

    Add mainwindow.cpp

---
+++ client/mainwindow.cpp
@@ -0,0 +1,163 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QFont>
+#include <QFontDialog>
+#include <QColor>
+#include <QColorDialog>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("Notepad dei Povery");
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    file_path = "";
+    ui->textEdit->setText("");
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString file_name = QFileDialog::getOpenFileName(this,"Open the file");
+    QFile file(file_name);
+    file_path = file_name;
+    if(!file.open(QFile::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "..", "file not open");
+        return;
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    if(file_path == ""){
+        on_actionSave_As_triggered();
+        return;
+    }else{
+    QFile file(file_path);
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "..", "file not saved!");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+    }
+}
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString file_name = QFileDialog::getSaveFileName(this,"Save the file");
+    QFile file(file_name);
+    file_path = file_name;
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "..", "file not saved!");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionAbout_us_triggered()
+{
+    QString about_text;
+      about_text  = "Authors: Isabella Romita, Debora Caldarola, Vito Tassielli, Simone Dutto\n";
+      about_text += "Date: 11/09/2019\n";
+      about_text += "(C) Notepad  (R)\n";
+
+      QMessageBox::about(this,"About Notepad",about_text);
+}
+
+void MainWindow::on_actionBold_triggered()
+{
+    if(ui->textEdit->fontWeight()!=75)
+        ui->textEdit->setFontWeight(75);
+    else
+        ui->textEdit->setFontWeight(50);
+}
+
+void MainWindow::on_actionItalic_triggered()
+{
+    if(ui->textEdit->fontItalic())
+        ui->textEdit->setFontItalic(false);
+    else
+        ui->textEdit->setFontItalic(true);
+}
+
+void MainWindow::on_actionUnderlined_triggered()
+{
+    if(ui->textEdit->fontUnderline())
+        ui->textEdit->setFontUnderline(false);
+    else
+        ui->textEdit->setFontUnderline(true);
+}
+
+void MainWindow::on_actionFont_triggered()
+{
+    bool ok;
+    QFont font = QFontDialog::getFont(&ok, this);
+    if(ok)
+        ui->textEdit->setFont(font);
+    else
+        return;
+}
+
+void MainWindow::on_actionColor_triggered()
+{
+    QColor color = QColorDialog::getColor(Qt::white,this,"Choose a color");
+    if(color.isValid())
+        ui->textEdit->setTextColor(color);
+}
+
+void MainWindow::on_actionBackgorund_Color_triggered()
+{
+    QColor color = QColorDialog::getColor(Qt::white,this,"Choose a color");
+    if(color.isValid())
+        ui->textEdit->setTextBackgroundColor(color);
+}
