0bd635f0859550b6597520e9ffabb2651c0060e9
s263626 <s263626@studenti.polito.it>
2019-09-19 18:33:27 +0200

    commit try

--- server/letter.h
+++ server/letter.h
@@ -1,12 +1,28 @@
 #ifndef LECTER_H
 #define LECTER_H
 
+#include <QVector>
 
 class Letter
 {
     // campi per gestire la lettera, la posizione, lo stile, etc
+private:
+    QChar letter;
+    //int siteID;
+    //int counter;
+    QVector<int> fractionalIndexes;
+
 public:
-    Letter();
+    //Letter(int socketID);
+    Letter(QChar letter, int index, QVector<int> fractionals);
+    QChar getLetterValue();
+    QVector<int> getFractionalIndexes();
+    int getIndex();
+    //int getCounter();
+    //void incrementCounter();
+    void setIndex(int index);
+    void addFractionalDigit(int value);
+    QChar getLetterID();
 };
 
 #endif // LECTER_H
