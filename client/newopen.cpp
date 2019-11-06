2ba29eca68b9fe3a14757cf9301c139a1c782002
Vito Tassielli <vitotassielli@gmail.com>
2019-11-06 17:47:28 +0100

    add newOpen Class

---
+++ client/newopen.cpp
@@ -0,0 +1,38 @@
+#include "newopen.h"
+#include "ui_newopen.h"
+
+NewOpen::NewOpen(Socket *sock, FileHandler *fHandler, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::NewOpen),
+    socket(sock),
+    fileHandler(fHandler)
+{
+    ui->setupUi(this);
+
+    for (QString s : this->socket->getListFiles()){
+        ui->listWidget->addItem(s);
+    }
+
+    connect( this, SIGNAL(openThisFile(QString)),
+             this->socket, SLOT(sendOpenFile(QString)));
+}
+
+NewOpen::~NewOpen()
+{
+    delete ui;
+}
+
+void NewOpen::on_pushButton_2_clicked() //Bottone: new Document
+{
+    mainwindow = new MainWindow(this->socket, this->socket->getFHandler(), this);
+    hide();
+    mainwindow->show();
+}
+
+void NewOpen::on_pushButton_clicked() //Bottone: open Document
+{
+    mainwindow = new MainWindow(this->socket, this->socket->getFHandler(), this);
+    mainwindow->show();
+    emit openThisFile(ui->listWidget->currentItem()->text());
+    hide();
+}
