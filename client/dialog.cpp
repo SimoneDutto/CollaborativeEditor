0f25d58ee8887b2874ecfbc7095ad8409b569b3c
SimoneDutto <simonedutto8@gmail.com>
2020-06-05 15:38:24 +0200

    Adjust Dialog

--- client/dialog.cpp
+++ client/dialog.cpp
@@ -20,25 +20,18 @@ Dialog::Dialog(Socket *sock, QWidget *parent) :
     p.setColor(QPalette::Base, QColor(209,209,214)); // set color "Red" for textedit base
     p.setColor(QPalette::Text, Qt::black); // set text color which is selected from color pallete
     ui->listWidget->setPalette(p);
-    ui->lineEdit->setPalette(p);
     this->show();
     setWindowTitle("Files");
-    ui->lineEdit->setStyleSheet(":focus {border: 2px solid #ace4c6};");
-    ui->lineEdit->setAttribute(Qt::WA_MacShowFocusRect,0);
 
     QIcon *user_icon= new QIcon(":/rec/icone/icons8-aggiungi-collegamento-96.png");
-    ui->pushButton_3->setIcon(*user_icon);
-    ui->pushButton_3->setIconSize(QSize(30, 30));
     user_icon= new QIcon(":/rec/icone/icons8-aggiungi-file-96.png");
     ui->pushButton_4->setIcon(*user_icon);
     ui->pushButton_4->setIconSize(QSize(30, 30));
 
     QString styleSheet = "QPushButton {background-color: transparent; border-style: none; color: white}";
-    ui->pushButton_3->setStyleSheet(styleSheet);
     ui->pushButton_4->setStyleSheet(styleSheet);
 
     ui->pushButton->setStyleSheet("color: white; background-color: #706d82;  border-radius:5px");
-    ui->pushButton_2->setStyleSheet("color: white; background-color: #706d82;  border-radius:5px");
     /*
     listFiles.append("Prova1");
     listFiles.append("Prova2");
@@ -53,12 +46,6 @@ Dialog::Dialog(Socket *sock, QWidget *parent) :
 
     connect( this, SIGNAL(openThisFile(QString)),
              this->socket, SLOT(sendOpenFile(QString)));
-    connect(this, SIGNAL(checkUri(QString)),
-            this->socket, SLOT(sendAccess(QString)));
-    connect(socket, SIGNAL(uriIsOk(QString)),
-             this, SLOT(uriIsOk(QString)));
-    connect(socket, SIGNAL(uriIsNotOk()),
-             this, SLOT(uriIsNotOk()));
     connect(this, SIGNAL(destroyM(QString)), parent, SLOT(destroyMain(QString)));
 }
 
@@ -77,19 +64,3 @@ void Dialog::on_pushButton_clicked()
     hide();
 }
 
-void Dialog::on_pushButton_2_clicked()
-{
-    QString uri = ui->lineEdit->text();
-    emit checkUri(uri);
-}
-
-void Dialog::uriIsOk(QString uri){
-    ui->listWidget->addItem(uri);
-    ui->listWidget->findItems(uri,Qt::MatchExactly).first()->setBackground(QColor(52,199,89));
-
-}
-
-void Dialog::uriIsNotOk(){
-    Uri *uri = new Uri(socket,this);
-    uri->show();
-}
